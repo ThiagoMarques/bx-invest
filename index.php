88a82ada3d17e2f804439d6d208d13b8a0a47254
thiagomarques <thiagossmarques@gmail.com>
2019-11-01 00:41:43 -0300

    Ajustes gerais

--- index.php
+++ index.php
@@ -2,139 +2,178 @@
 <html lang="pt-br">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="BX Invest">
-  <meta name="author" content="Gama XP 26">
-  <meta name="generator" content="Jekyll v3.8.5">
-  <link rel="icon" href="assets/imgs/logo.png">
-  <title>BX Invest</title>
-  <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/product/">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-  <link href="assets/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link href="assets/css/main.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="BX Invest">
+    <meta name="author" content="Gama XP 26">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <link rel="icon" href="assets/imgs/logo.png">
+    <title>BX Invest</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/product/">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="assets/css/main.css" rel="stylesheet">
 </head>
 
 <body>
-  <!-- NavBar -->
-
-  <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
-    <header class="masthead mb-auto">
-      <div class="inner">
-        <nav class="nav nav-masthead justify-content-center">
-          <a class="nav-link text-white menu text-uppercase" href="#">Início</a>
-          <a class="nav-link text-white menu text-uppercase" href="#">Descubra seu Perfil</a>
-          <a class="nav-link text-white menu text-uppercase" href="#">Suporte</a>
-        </nav>
-      </div>
-    </header>
-
-    <!-- Linha principal [ESQ | DIR] -->
-    <div class="card-principal">
-      <div class="row">
-        <div class="col-md-6">
-          <h1 class="texto-principal">Qual meu perfil de</h1>
-          <h1 class="texto-principal-enfase">investidor?</h1>
-          <p class="texto-descritivo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam est libero,
-            eget blandit neque
-            ullamcorper vitae.</p>
-          <button class="btn-bx text-white">QUERO DESCOBRIR</button>
+    <!-- NavBar -->
+    <div class="bx-principal">
+        <div class="index">
+            <div class="content-principal">
+                <header class="masthead mb-auto">
+                    <div class="inner">
+                        <nav class="nav nav-masthead justify-content-center">
+                            <a class="nav-link text-white menu text-uppercase" href="#">Início</a>
+                            <a class="nav-link text-white menu text-uppercase" href="#">Descubra seu Perfil</a>
+                            <a class="nav-link text-white menu text-uppercase" href="#">Suporte</a>
+                        </nav>
+                    </div>
+                </header>
+                <!-- Linha principal [ESQ | DIR] -->
+                <div class="card-principal">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h1 class="texto-principal">Qual meu perfil de</h1>
+                            <h1 class="texto-principal-enfase">investidor?</h1>
+                            <p class="texto-descritivo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                                aliquam
+                                est libero,
+                                eget blandit neque
+                                ullamcorper vitae.</p>
+                            <button class="btn-bx text-white">QUERO DESCOBRIR</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-md-6">
-          <img class="d-none d-sm-block align-top img-fluid" src="assets/imgs/index-image.png" alt="#IndexImage">
+        <!-- Card da Simulação -->
+        <div class="container-bx">
+            <div class="card-principal">
+                <div class="container section">
+                    <div class="row justify-content-center">
+                        <h1 class="texto-simulacao text-center">SIMULADOR DE PERFIL INVESTIDOR</h1>
+                        <div class="col-sm-8 align-self-center">
+                            <form action="" method="post" class="formulario">
+                                <div class="row">
+                                    <div class="col-md-6 form-col">
+                                        <input type="text" name="txtNome" class="form-control" id="txtNome"
+                                            placeholder="Seu nome">
+                                    </div>
+                                    <div class="col-md-6 form-col">
+                                        <input type="email" name="txtMail" class="form-control" id="txtMail"
+                                            placeholder="Seu e-mail">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6 form-col">
+                                        <input type="text" name="txtCapital" class="form-control" id="txtCapital"
+                                            placeholder="Capital (em BRL)">
+                                    </div>
+                                    <div class="col-md-6 form-col">
+                                        <input type="text" name="txtLiquidez" class="form-control" id="txtLiquidez"
+                                            placeholder="Liquidez (em BRL)">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6 form-col">
+                                        <input type="text" name="txtAporte" class="form-control" id="txtAporte"
+                                            placeholder="Aporte (em BRL)">
+                                    </div>
+                                    <div class="col-md-6 form-col">
+                                        <select class="form-control" name="txtPerfil" id="txtPerfil" >
+                                            <option disabled selected>Selecione seu perfil</option>
+                                            <option>Geral</option>
+                                            <option>Qualificado</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6 form-col">
+                                    <select class="form-control" name="txtPublico" id="txtPublico" >
+                                            <option disabled selected>Selecione o público</option>
+                                            <option>Geral</option>
+                                            <option>Qualificado</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-6 form-col">
+                                    <select class="form-control" name="txtTipo" id="txtTipo" >
+                                            <option disabled selected>Selecione o tipo</option>
+                                            <option>Pessoa física</option>
+                                            <option>Pessoa jurídica</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12 form-col">
+                                        <textarea class="form-control" name="txtMensagem" id="txtMensagem" rows="3"
+                                            placeholder="Escreva sua mensagem"></textarea>
+                                    </div>
+                                </div>
+                            </form>
+                            <button type="submit" name="Enviar"
+                                    class="btn btn-success btn-formulario btn-block btn-lg">DESCUBRA SEU PERFIL</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card do Resultado -->
+            <div class="container-bx">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card-resultado">
+                            <h1 class="texto-resultado text-white">Resultado:</h1>
+                            <p class="texto-descritivo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                                aliquam est
+                                libero, eget blandit neque ullamcorper vitae. Lorem ipsum dolor sit amet, consectetur
+                                adipiscing
+                                elit. Sed aliquam est libero, eget blandit neque ullamcorper vitae. Lorem ipsum dolor
+                                sit amet,
+                                consectetur adipiscing elit. Sed aliquam est libero, eget blandit neque ullamcorper
+                                vitae. Lorem
+                                ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam est libero, eget blandit
+                                neque
+                                ullamcorper vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam
+                                est libero,
+                                eget blandit neque ullamcorper vitae. Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Sed
+                                aliquam est libero, eget blandit neque ullamcorper vitae..</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card Final -->
+            <div class="card-final">
+                <div class="row justify-content-center">
+                    <div class="container-card">
+                        <div class="col-md-12">
+                            <p class="texto-final">Agora que você já conhece qual seu perfil de investidor…</p>
+                            <button class="section btn-final btn-lg btn-block text-white">CHAMADA PARA O SITE DO
+                                PARCEIRO</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
+
+        <!-- Rodapé -->
+        <footer class="mastfoot mt-auto">
+            <div class="inner">
+                <p>@BXInvest 2019</p>
+            </div>
+        </footer>
     </div>
 
-    <!-- Card da Simulação -->
-    <div class="card-principal">
-      <div class="container section">
-        <div class="row justify-content-center">
-          <h1 class="texto-simulacao">SIMULADOR DE PERFIL INVESTIDOR</h1>
-          <div class="col-sm-8 align-self-center">
-            <form action="" method="post" class="formulario">
-              <div class="row">
-                <div class="col-md-6 form-col">
-                  <input type="text" name="txtNome" class="form-control" id="txtNome" placeholder="Seu nome">
-                </div>
-                <div class="col-md-6 form-col">
-                  <input type="email" name="txtMail" class="form-control" id="txtMail" placeholder="Seu e-mail">
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6 form-col">
-                  <input type="text" name="txtPatrimonio" class="form-control" id="txtPatrimonio" placeholder="Patrimônio (em BRL)">
-                </div>
-                <div class="col-md-6 form-col">
-                  <input type="text" name="txtLiquidez" class="form-control" id="txtLiquidez" placeholder="Liquidez (em BRL)">
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6 form-col">
-                  <input type="text" name="txtNome" class="form-control" id="txtNome" placeholder="Nome">
-                </div>
-                <div class="col-md-6 form-col">
-                  <input type="text" name="txtNome" class="form-control" id="txtNome" placeholder="Nome">
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6 form-col">
-                  <input type="text" name="txtNome" class="form-control" id="txtNome" placeholder="Nome">
-                </div>
-                <div class="col-md-6 form-col">
-                  <input type="text" name="txtNome" class="form-control" id="txtNome" placeholder="Nome">
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-12 form-col">
-                  <textarea class="form-control" name="txtMensagem" id="txtMensagem" rows="3"
-                    placeholder="Escreva sua mensagem"></textarea>
-                </div>
-              </div>
-              <button type="submit" name="Enviar" class="btn btn-success btn-block btn-lg">Enviar</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Card do Resultado -->
-    <div class="card-resultado">
-      <div class="container-card">
-        <h1 class="texto-resultado text-white">Resultado:</h1>
-        <p class="texto-descritivo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam est libero, eget blandit neque ullamcorper vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam est libero, eget blandit neque ullamcorper vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam est libero, eget blandit neque ullamcorper vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam est libero, eget blandit neque ullamcorper vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam est libero, eget blandit neque ullamcorper vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam est libero, eget blandit neque ullamcorper vitae..</p>
-      </div>
-    </div>
-
-    <!-- Card Final -->
-    <div class="card-final">
-      <div class="row justify-content-center">
-        <div class="container-card">
-          <div class="col-md-12">
-            <p class="texto-final">Agora que você já conhece qual seu perfil de investidor…</p>
-            <button class="section btn-final btn-lg btn-block text-white">CHAMADA PARA O SITE DO PARCEIRO</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Rodapé -->
-    <footer class="mastfoot mt-auto">
-      <div class="inner">
-        <p>@BXInvest 2019</p>
-      </div>
-    </footer>
-  </div>
-
-  <!-- Scripts do Site -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-    crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-  <script src="assets/js/vendor/popper.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Scripts do Site -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
 </body>
 
 </html>
